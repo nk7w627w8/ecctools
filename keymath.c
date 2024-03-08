9ae3dac056771ff44651d1f99b83a75b096fcbb1
nk7w627w8 <162332181+nk7w627w8@users.noreply.github.com>
2024-03-08 22:14:44 +0530

    Update keymath.c

--- keymath.c
+++ keymath.c
@@ -136,7 +136,7 @@ int main(int argc, char **argv)  {
 			else	{
 				mpz_invert(inversemultiplier,number,EC.n);
 				Scalar_Multiplication_custom(A,&C,inversemultiplier);
-				if(void (Point_Addition(&A,&B,&C))) {
+				if(number,number,EC.n) {
 					printf("Result: %s\n\n # right",str_publickey);
 				}
 				else {
