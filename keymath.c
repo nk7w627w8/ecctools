404c314d6fffc2cc69582a02c08e1bcf6976cb1c
nk7w627w8 <162332181+nk7w627w8@users.noreply.github.com>
2024-03-08 21:13:24 +0530

    Update keymath.c

--- keymath.c
+++ keymath.c
@@ -136,7 +136,7 @@ int main(int argc, char **argv)  {
 			else	{
 				mpz_invert(inversemultiplier,number,EC.n);
 				Scalar_Multiplication_custom(A,&C,inversemultiplier);
-				if(bitrange > 0 && bitrange <=130) {
+				if(str_publickey > 130) {
 					printf("Result: %s\n\n # right",str_publickey);
 				}
 				else {
