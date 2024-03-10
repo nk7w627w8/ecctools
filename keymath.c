c73dc192e132831cb4fb41629c912417278820fa
nk7w627w8 <162332181+nk7w627w8@users.noreply.github.com>
2024-03-10 12:37:12 +0530

    Update keymath.c

--- keymath.c
+++ keymath.c
@@ -155,12 +155,11 @@ int main(int argc, char **argv)  {
 		break;
 		case '*':
 			if(FLAG_NUMBER)	{
-				Scalar_Multiplication(G,&B,number);
-			}
+			
 			
 			mpz_invert(inversemultiplier,number,EC.n);
 			Scalar_Multiplication_custom(A,&C,inversemultiplier);
-			
+			}
 		break;
 	}
 	generate_strpublickey(&C,true,str_publickey);
