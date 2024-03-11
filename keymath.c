6eff933cc4f698c29d67beca435e3f5849336fee
nk7w627w8 <162332181+nk7w627w8@users.noreply.github.com>
2024-03-11 07:34:51 +0530

    Update keymath.c

--- keymath.c
+++ keymath.c
@@ -51,7 +51,6 @@ char str_address[41];
 struct Point A,B,C;
 
 int FLAG_NUMBER = 0;
-int divide = 0;
 
 mpz_t inversemultiplier,divide,number;
 
@@ -111,7 +110,7 @@ int main(int argc, char **argv)  {
 			FLAG_NUMBER = 1;
 		break;
 	}
-	mpz_mod(number,number,EC.n);
+	mpz_mod(number,number,divide,EC.n);
 	switch(argv[2][0])	{
 		case '+':
 			if(FLAG_NUMBER)	{
